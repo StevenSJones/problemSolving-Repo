1e417e119083c97d3d7090118fab3e7e87040a5e
SS Jones <SSJones.code@gmail.com>
2020-12-14 16:34:18 -0700

    I incorporated a switch statement inside a function to print the current day to the console.

--- script.js
+++ script.js
@@ -46,6 +46,39 @@ function fizzBuzzSwitch(n) {
 }
 
 fizzBuzzSwitch(65);
+//---------------------------------------
+
+todaysDate = new Date().getDay();
+console.log(todaysDate);
+
+function GatherDateSwitch (todaysDate) {
+    switch (todaysDate){
+        case 0: date = "Sunday";
+        console.log('Today is Sunday!');
+        break;
+        case 1: date = "Monday";
+        console.log('Today is Monday!');
+        break;
+        case 2: date = "Tuesday";
+        console.log('Today is Tuesday!');
+        break;
+        case 3: date = "Wednesday";
+        console.log('Today is Wednesday!');
+        break;
+        case 4: date = "Thursday";
+        console.log('Today is Thursday!');
+        break;
+        case 5: date = "Friday";
+        console.log('Today is Friday!');
+        break;
+        case 6: date = "Saturday";
+        console.log('Today is Saturday!');
+        break;
+    }
+}
+
+GatherDateSwitch(todaysDate);
+
 
 
 
