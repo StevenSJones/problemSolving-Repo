480326b8e5983320911a71f89f9043db8298fa1f
SS Jones <SSJones.code@gmail.com>
2020-12-24 18:01:14 -0700

    I cut down on the code for the input by including a placeholder and removing the excess h5 element.

--- script.js
+++ script.js
@@ -84,6 +84,15 @@ function GatherDateSwitch(todaysDate) {
   }
 }
 
+//welcome name functionality with a doc,ready function first followed by jQuery for the name and event handler, followed by the action to be taken in the form of a callback function:
+$(function() {
+  $("#name").keyup(function() {
+      $("#msg").html($("#name").val());
+  });
+});
+
+//-------------------------------------------------
+
 GatherDateSwitch(todaysDate);
 
 // The Math.random() function is used to return a floating-point pseudo-random number between range [0,1) , 0 (inclusive) and 1 (exclusive).This random number can then be scaled according to the desired range.
